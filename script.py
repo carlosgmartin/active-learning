21a704721cd8f78eb80f77a3c6d996ef7e7982a1
Carlos Martin <carlos.martin@columbia.edu>
2016-10-13 18:04:18 -0400

    Calculating likelihoods and posteriors according to Bayesian Hierarchical Clustering algorithm

--- script.py
+++ script.py
@@ -2,10 +2,18 @@ import numpy as np
 from sklearn.datasets import load_digits
 from sklearn.cluster import AgglomerativeClustering
 from sklearn.metrics import adjusted_rand_score
+from scipy.stats import binom
+from scipy.integrate import quad
+from collections import deque
 
 
 # Load the digits dataset
 digits = load_digits()
+
+# Make this a binary classification problem
+digits.target = digits.target < 5
+
+
 
 
 # Find the target categories
@@ -36,13 +44,41 @@ for parent, children in enumerate(clustering.children_, clustering.n_leaves_):
 	for child in children:
 		get_parent[child] = parent
 
+for leaf in range(clustering.n_leaves_):
+	get_children[leaf] = []
+
+
+
+# Returns the traversal of a tree
+def traverse(node):
+	traversal = []
+
+	deq = deque()
+	deq.append(node)
+	while len(deq) > 0:
+		node = deq.popleft() # pop = DFS, popleft = BFS
+		traversal.append(node)
+		for child in get_children[node]:
+			deq.append(child)
+
+	return traversal
+
+# Find the root of the tree
+root = len(clustering.children_) + clustering.n_leaves_ - 1
+
+# Find the tree traversal
+traversal = traverse(root)
+
+
 
 # Store the counts of each category for each node
 get_counts = {}
-for node in range(clustering.n_leaves_ + len(clustering.children_)):
+for node in traversal:
 	get_counts[node] = {}
 	for category in categories:
 		get_counts[node][category] = 0
+
+
 
 
 def query(leaf):
@@ -61,15 +97,37 @@ def query(leaf):
 			break
 
 
-
 # Query the category of each leaf
 for leaf in range(clustering.n_leaves_):
 	query(leaf)
 
 
-root = len(clustering.children_) + clustering.n_leaves_ - 1
-print get_counts[root]
 
+
+# Returns the marginal likelihood (or model evidence) of a binomial distribution
+def marginal_likelihood(successes, trials):
+	return quad(lambda probability: binom.pmf(successes, trials, probability), 0, 1)[0]
+
+
+
+
+get_likelihood = {}
+get_posterior = {}
+
+# Print the marginal likelihood of each node
+for node in reversed(traversal):
+
+	trials = sum(get_counts[node].values())
+	successes = get_counts[node][0]
+
+	get_likelihood[node] = marginal_likelihood(successes, trials)
+	
+	get_posterior[node] = .5 * get_likelihood[node] + .5 * np.product([get_posterior[child] for child in get_children[node]])
+
+	print '{:.2f}'.format(get_likelihood[node])
+	print '{:.2f}'.format(get_posterior[node])
+
+	print
 
 
 
