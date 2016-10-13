8aacdb9c14e70985a4b456e88e244222a53a2372
Carlos Martin <carlos.martin@columbia.edu>
2016-10-13 15:32:43 -0400

    Example clustering on digits data

--- script.py
+++ script.py
@@ -1,3 +1,11 @@
+import numpy as np
 from sklearn.datasets import load_digits
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics import adjusted_rand_score
 
 digits = load_digits()
+
+clustering = AgglomerativeClustering(n_clusters=len(np.unique(digits.target)))
+clustering.fit(digits.data)
+
+print adjusted_rand_score(clustering.labels_, digits.target)
